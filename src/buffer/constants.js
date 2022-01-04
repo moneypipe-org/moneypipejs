ed80b8c2e730906bdd5fc17e9cf33d45e36be664
skogard <skogard@protonmail.com>
2022-01-04 09:57:00 -0500

    mainnet release

--- src/buffer/constants.js
+++ src/buffer/constants.js
@@ -1,6 +1,7 @@
 module.exports = {
   factory: {
-    rinkeby: "0x6706c0452C82f1d57d25860A129627210438c075"
+    rinkeby: "0x6706c0452C82f1d57d25860A129627210438c075",
+    main: "0xA9183BD6FFf2d50398e95a1Bf2FaA98Aff2FBE76"
   },
   ipfs: "https://{{cid}}.ipfs.dweb.link"
 }
--- src/stream/constants.js
+++ src/stream/constants.js
@@ -1,5 +1,6 @@
 module.exports = {
   factory: {
-    rinkeby: "0x18fA6B478147aA7E2d50828251d57045fa030593"
+    rinkeby: "0x18fA6B478147aA7E2d50828251d57045fa030593",
+    main: "0x88891017548DCFf014EFFAe944D4AB7d2E5Cd8A8"
   }
 }
